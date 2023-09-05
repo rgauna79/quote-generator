0350a759a1b6e59e4a1ec9890d8611b30d49da8e
rgauna79 <rgauna@gmail.com>
2023-09-04 18:56:29 -0600

    Api quotes request

--- src/App.js
+++ src/App.js
@@ -1,45 +1,13 @@
-// App.js
-
 import React from "react";
-import axios from "axios";
+import Quotes from './components/Quotes'
+import './App.css';
 
-import "./App.css";
-
-class App extends React.Component {
-	state = { advice: "" };
-
-	componentDidMount() {
-		this.fetchAdvice();
-	}
-
-	fetchAdvice = () => {
-		axios
-			.get("https://api.adviceslip.com/advice")
-			.then((response) => {
-				const { advice } = response.data.slip;
-
-				this.setState({ advice });
-			})
-
-			.catch((error) => {
-				console.log(error);
-			});
-	};
-
-	render() {
-		const { advice } = this.state;
-
-		return (
-			<div className="app">
-				<div className="card">
-					<h1 className="heading">{this.state.advice}</h1>
-					<button className="button" onClick={this.fetchAdvice}>
-						<span>Give Me Advice</span>
-					</button>
-				</div>
-			</div>
-		);
-	}
+function App() {
+	return (
+		<div className="App">
+		<Quotes />	
+		</div>
+	)
 }
 
 export default App;
---
+++ src/components/Quotes.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import './quotes.css';
+//App component
+
+class Quotes extends React.Component{
+    constructor(){
+        super();
+        this.state = ({
+
+        });
+    }
+
+    render(){
+
+        return(
+            <div>
+                <QuoteBox />
+            </div>
+        );
+    }
+}
+
+class QuoteBox extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            quoteText: "",
+            quoteAuthor: "",
+            color: this.props.color,
+            tweetUrl: "https://twitter.com/intent/tweet/?text="
+        }
+    this.getNewQuote();
+
+    //Bind Handles and functions
+    this.getNewQuote = this.getNewQuote.bind(this);
+    this.handleNewQuote = this.handleNewQuote.bind(this);
+    }
+
+    //Get a quote and author from API and set state
+    getNewQuote(){
+        const app = this;
+        fetch('https://api.quotable.io/random')
+        .then((response) => response.json())
+        .then((data) => {
+            app.setState ({
+                quoteText: data.content,
+                quoteAuthor: data.author,
+                tweetUrl: "https://twitter.com/intent/tweet/?text=" + data.content.replace(/ /g, "+")
+            });
+        });
+    }
+
+    //Handles new quote button
+    handleNewQuote(){
+
+        this.getNewQuote();
+    }
+
+    render(){
+        return (
+            <main id="quote-box">
+                <div id="quote-content">
+                    <div id="text">{this.state.quoteText}</div>
+                    <div id="author">{this.state.quoteAuthor}</div>
+                </div>
+                <button onClick={this.handleNewQuote} id="new-quote">Get New quote</button>
+
+
+            </main>
+
+        )
+    }
+
+}
+
+
+
+export default Quotes;
